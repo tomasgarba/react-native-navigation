fe6bc685d070330e3ecbb489984d6a1b913c37d0
Guy Carmeli <guyc@wix.com>
2017-06-14 18:36:15 +0300

    MainActivity implements NavigationActivity instead of SplashActivity

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example;
 
-import com.reactnativenavigation.controllers.SplashActivity;
+import com.reactnativenavigation.controllers.NavigationActivity;
 
-public class MainActivity extends SplashActivity {
+public class MainActivity extends NavigationActivity {
 
 }
